6898e7f52ae41e3b0182d7454c563fb991ee571b
akshaygohel01 <uselocal14@gmail.com>
2023-06-13 23:19:12 +0530

    FINAL COMMIT

---
+++ frontend/build/precache-manifest.e0028fd6cd4956385e4144c4302fba57.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e43e4914d88ee98750ee",
+    "url": "/static/css/main.3f112223.chunk.css"
+  },
+  {
+    "revision": "e43e4914d88ee98750ee",
+    "url": "/static/js/main.e43e4914.chunk.js"
+  },
+  {
+    "revision": "72bad6ca2a6f58463679",
+    "url": "/static/js/1.72bad6ca.chunk.js"
+  },
+  {
+    "revision": "44790e1b441c984e66ff",
+    "url": "/static/css/2.8522a162.chunk.css"
+  },
+  {
+    "revision": "44790e1b441c984e66ff",
+    "url": "/static/js/2.44790e1b.chunk.js"
+  },
+  {
+    "revision": "44c51d9554b699d36374",
+    "url": "/static/js/runtime~main.44c51d95.js"
+  },
+  {
+    "revision": "dad7495a0bafdbd6f113658060d1b8e9",
+    "url": "/static/media/docimg1.dad7495a.jpg"
+  },
+  {
+    "revision": "71926a467ff803e749455bf6029ce3b5",
+    "url": "/static/media/logo.71926a46.png"
+  },
+  {
+    "revision": "95fc00899ad26341b825d459beeaa823",
+    "url": "/index.html"
+  }
+];
